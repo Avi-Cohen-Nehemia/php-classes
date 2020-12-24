02d8f70984689a18756aa62f50534ee7e2e8239e
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-24 16:19:27 +0000

    php classes - solved exercise 2

---
+++ challenges/exercise-02.php
@@ -0,0 +1,40 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// Create a class that represents a light switch
+
+class LightSwitch
+{
+    private $on = false;
+
+    public function isOn() : bool
+    {
+        return $this->on;
+    }
+
+    public function turnOn() : LightSwitch
+    {
+        $this->on = true;
+        return $this;
+    }
+
+    public function turnOff() : LightSwitch
+    {
+        $this->on = false;
+        return $this;
+    }
+}
+
+$lightSwitch = new LightSwitch();
+
+// you can check whether it is on or not
+dump($lightSwitch->isOn()); // false
+
+// you can turn it on
+$lightSwitch->turnOn();
+dump($lightSwitch->isOn()); // true
+
+// you can turn it off
+$lightSwitch->turnOff();
+dump($lightSwitch->isOn()); // false
