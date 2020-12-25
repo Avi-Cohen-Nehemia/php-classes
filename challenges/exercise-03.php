2ab69556862ea25fa806223d2b1b2d4babe358a9
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-25 11:51:38 +0000

    php classes - solved exercise 3

---
+++ challenges/exercise-03.php
@@ -0,0 +1,54 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// Create a class that represents a car
+
+class Car
+{
+    private $make;
+    private $numberPlate;
+    private $mileage = 0;
+
+    public function __construct(string $make, string $numberPlate)
+    {
+        $this->make = $make;
+        $this->numberPlate = $numberPlate;
+    }
+
+    public function getNumberPlate() : string
+    {
+        return $this->numberPlate;
+    }
+
+    public function getMake() : string
+    {
+        return $this->make;
+    }
+
+    public function getMileage() : int
+    {
+        return $this->mileage;
+    }
+
+    public function addJourney(int $miles) : Car
+    {
+        $this->mileage += $miles;
+        return $this;
+    }
+}
+
+// you pass in a make and number plate
+$car = new Car("Ford", "XY11 4TY");
+
+// you can get various bits of information about it
+dump($car->getNumberplate()); // "XY11 4TY"
+dump($car->getMake()); // "Ford"
+dump($car->getMileage()); // 0
+
+// you can add journey
+$car->addJourney(100);
+dump($car->getMileage()); // 100
+
+$car->addJourney(200);
+dump($car->getMileage()); // 300
